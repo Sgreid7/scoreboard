8eecc80965d7a06dac2c94f20aa9d171ce3421e3
Sam Greider <sam.greider7@gmail.com>
2020-05-06 11:06:21 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Scoreboard
+
+This is a web app build with JavaScript that allows us to keep track of the score of two teams up to 21 at which the team will win. You may increment up to 3 periods and reset the game with the reset button
+
+# Objectives
+
+- Become familiar with DOM element selection
+- Work with event listeners
+- Add classes via Javascript
+
+# Includes
+
+- [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+- [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+- [JAVASCRIPT](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+# Requirements
+
+- Implement the design using the given HTML & CSS.
+- Assign click events to the update buttons to update the corresponding values on the page. - The score buttons should adjust the current score by a "hard-coded" value
+- Add an if statement to stop each team's score from going below 0.
+- Add an if statement to stop each team's score from going above 21
+- When a team gets 21 points, disable the buttons and display a message to the user of who won
+- Add a reset button that resets the scoreboard back to each team having 0 points and re-enables the buttons
+- Add more CSS to the winning teams score to indicate who won
+- Implement a Quarter/Period Tracker
+
+# Featured Code
+
+```JSX
+const resetGame = () => {
+  document.querySelector('.team-1-score').textContent = 0
+  document.querySelector('.team-2-score').textContent = 0
+  const buttons = document.querySelectorAll('button')
+  buttons.forEach(button => (button.disabled = false))
+  document.querySelector('.team1-end-game-message').textContent = ''
+  document.querySelector('.team2-end-game-message').textContent = ''
+  document.querySelector('.team-1-score').classList.remove('winner')
+  document.querySelector('.team-1-score').classList.remove('loser')
+  document.querySelector('.team-2-score').classList.remove('winner')
+  document.querySelector('.team-2-score').classList.remove('loser')
+}
+```
+
+# Live Site
+
+- [LIVE SITE](https://scoreboard-sam.netlify.app/)
--- public/main.js
+++ public/main.js
@@ -1,5 +1,3 @@
-const updateButton1 = document.querySelector('.update-team-1-name')
-const updateButton2 = document.querySelector('.update-team-2-name')
 const addButton1 = document.querySelector('.team-1-add-1-button')
 const subtractButton1 = document.querySelector('.team-1-subtract-1-button')
 const addButton2 = document.querySelector('.team-2-add-1-button')
@@ -7,17 +5,20 @@ const subtractButton2 = document.querySelector('.team-2-subtract-1-button')
 const resetButton = document.querySelector('.reset-button')
 const addPeriod = document.querySelector('.add-period')
 
-const updateTeamName1 = () => {
-  const newTeamName1 = document.querySelector('.team-1-input').value
-  document.querySelector('.team-1-name').textContent = newTeamName1
+const updateTeamName = team => {
+  const newTeamName = document.querySelector('.team-' + team + '-input').value
+  document.querySelector('.team-' + team + '-name').textContent = newTeamName
 }
 
-const updateTeamName2 = () => {
-  const newTeamName2 = document.querySelector('.team-2-input').value
-  document.querySelector('.team-2-name').textContent = newTeamName2
-}
+document.querySelector('.update-team-1-name').addEventListener('click', () => {
+  updateTeamName('1')
+})
 
-const addTeam1 = () => {
+document.querySelector('.update-team-2-name').addEventListener('click', () => {
+  updateTeamName('2')
+})
+
+const addTeam1 = team => {
   const currentScore = parseInt(
     document.querySelector('.team-1-score').textContent
   )
@@ -29,8 +30,8 @@ const addTeam1 = () => {
     const buttons = document.querySelectorAll('button')
     buttons.forEach(button => (button.disabled = true))
     resetButton.disabled = false
-    document.querySelector('.team1-end-game-message').textContent = 'Winner'
-    document.querySelector('.team2-end-game-message').textContent = 'Loser'
+    document.querySelector('.team-1-end-game-message').textContent = 'Winner'
+    document.querySelector('.team-2-end-game-message').textContent = 'Loser'
     document.querySelector('.team-1-score').classList.add('winner')
     document.querySelector('.team-2-score').classList.add('loser')
   }
@@ -75,12 +76,6 @@ const subtractTeam2 = () => {
   }
 }
 
-const subtract1Period = () => {
-  const currentPeriod = parseInt(document.querySelector('.period').textContent)
-  const newPeriod = currentPeriod - 1
-  document.querySelector('.period').textContent = newPeriod
-}
-
 const add1Period = () => {
   const currentPeriod = parseInt(document.querySelector('.period').textContent)
   const newPeriod = currentPeriod + 1
@@ -100,8 +95,8 @@ const resetGame = () => {
   document.querySelector('.team-2-score').classList.remove('loser')
 }
 
-updateButton1.addEventListener('click', updateTeamName1)
-updateButton2.addEventListener('click', updateTeamName2)
+// updateButton1.addEventListener('click', updateTeamName1)
+// updateButton2.addEventListener('click', updateTeamName2)
 addButton1.addEventListener('click', addTeam1)
 addButton2.addEventListener('click', addTeam2)
 subtractButton1.addEventListener('click', subtractTeam1)
